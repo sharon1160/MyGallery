02c0aa51abacb57270e6607eed11047b3dec6108
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-04-28 16:37:10 -0500

    feat: add function of taking photo

--- app/src/main/java/com/example/mygallery/MainActivity.kt
+++ app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -1,11 +1,151 @@
 package com.example.mygallery
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.view.LifecycleCameraController
+import androidx.core.content.ContextCompat
+import com.example.mygallery.databinding.ActivityMainBinding
+
+import android.Manifest
+import android.content.ContentValues
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.Media
+import androidx.camera.core.ImageCapture
+import androidx.core.app.ActivityCompat
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import android.widget.Toast
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.core.Preview
+import androidx.camera.core.CameraSelector
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.concurrent.futures.await
+import androidx.core.content.PermissionChecker
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+
+    //private lateinit var cameraController: LifecycleCameraController
+
+    private var imageCapture: ImageCapture? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        if (!hasPermissions(baseContext)) {
+            // Request camera-related permissions
+            activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+        } else {
+            lifecycleScope.launch {
+                startCamera()
+            }
+        }
+
+        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
     }
+
+    private suspend fun startCamera() {
+        val cameraProvider = ProcessCameraProvider.getInstance(this).await()
+        val preview = Preview.Builder().build()
+        preview.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+
+        imageCapture = ImageCapture.Builder().build()
+
+        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+        try {
+            cameraProvider.unbindAll()
+            var camera = cameraProvider.bindToLifecycle(
+                this, cameraSelector, preview, imageCapture
+            )
+        } catch (e: Exception) {
+            Log.e(TAG, "UseCase binding failed", e)
+        }
+    }
+
+    private fun takePhoto() {
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-image")
+            }
+        }
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        ).build()
+
+        imageCapture?.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val msg = "Photo capture succeeded ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            }
+        )
+    }
+
+    private val activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
+        { permissions ->
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.key in REQUIRED_PERMISSIONS && it.value == false)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                Toast.makeText(this, "Permission request denied", Toast.LENGTH_LONG).show()
+            } else {
+                lifecycleScope.launch {
+                    startCamera()
+                }
+            }
+        }
+
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(
+                Manifest.permission.CAMERA
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+
+        fun hasPermissions(context: Context) = REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
 }
