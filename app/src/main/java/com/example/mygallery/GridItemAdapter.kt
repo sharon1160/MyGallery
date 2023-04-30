76f624526a8368333f49cf6e0aff16d653e76415
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-04-30 08:08:39 -0500

    feat: add grid item adapter

--- app/src/main/java/com/example/mygallery/GridItemAdapter.kt
+++ app/src/main/java/com/example/mygallery/GridItemAdapter.kt
@@ -1,4 +1,28 @@
 package com.example.mygallery
 
-class GridItemAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class GridItemAdapter(val images: Array<String>): RecyclerView.Adapter<GridItemAdapter.ViewHolder> () {
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val image :  ImageView = itemView.findViewById(R.id.photo)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.grid_item_view,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Picasso.get().load(images[position]).into(holder.image)
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
 }
