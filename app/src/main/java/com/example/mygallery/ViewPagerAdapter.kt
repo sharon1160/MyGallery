6344f4f6d6f50fbff580b281b3e749457b50a17f
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-01 17:31:25 -0500

    feat: add view pager adapter

--- app/src/main/java/com/example/mygallery/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/mygallery/ViewPagerAdapter.kt
@@ -1,4 +1,31 @@
 package com.example.mygallery
 
-class ViewPagerAdapter {
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ViewPagerAdapter(private var images: ArrayList<String>): RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>() {
+
+    inner class Pager2ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val itemImage: ImageView = itemView.findViewById(R.id.slider_photo)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewPagerAdapter.Pager2ViewHolder {
+        return Pager2ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_page, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerAdapter.Pager2ViewHolder, position: Int) {
+        holder.itemImage.setImageURI(Uri.parse(images[position]))
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
 }
