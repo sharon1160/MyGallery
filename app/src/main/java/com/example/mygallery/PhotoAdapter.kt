e96b333591e321431b2bab33fb9552678ced0505
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-02 16:49:36 -0500

    feat: change viewpager to recyclerview with snaphelper

--- app/src/main/java/com/example/mygallery/PhotoAdapter.kt
+++ app/src/main/java/com/example/mygallery/PhotoAdapter.kt
@@ -7,25 +7,26 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 
-class ViewPagerAdapter(private var images: ArrayList<String>): RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>() {
-
-    inner class Pager2ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+class PhotoAdapter(private var images: ArrayList<String>) :
+    RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>() {
+    class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val itemImage: ImageView = itemView.findViewById(R.id.slider_photo)
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): ViewPagerAdapter.Pager2ViewHolder {
-        return Pager2ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_page, parent, false))
+    ): PhotoAdapter.PhotoViewHolder {
+        return PhotoViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_page, parent, false)
+        )
     }
 
-    override fun onBindViewHolder(holder: ViewPagerAdapter.Pager2ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: PhotoAdapter.PhotoViewHolder, position: Int) {
         holder.itemImage.setImageURI(Uri.parse(images[position]))
     }
 
     override fun getItemCount(): Int {
         return images.size
     }
-
 }
--- app/src/main/java/com/example/mygallery/fragments/SliderFragment.kt
+++ app/src/main/java/com/example/mygallery/fragments/SliderFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
-import com.example.mygallery.ViewPagerAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.SnapHelper
+import com.example.mygallery.PhotoAdapter
 import com.example.mygallery.databinding.FragmentSliderBinding
 
 class SliderFragment : Fragment() {
@@ -19,14 +21,14 @@ class SliderFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSliderBinding.inflate(inflater, container, false)
+
         val imagesList: ArrayList<String> = arguments?.getStringArrayList("imagesList") ?: arrayListOf()
 
-        val viewPager: ViewPager2 = binding.slider
-        viewPager.adapter = ViewPagerAdapter(imagesList)
-        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        binding.slider.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL,false)
+        binding.slider.adapter = PhotoAdapter(imagesList)
 
-        binding.indicator.setViewPager(viewPager)
-
+        val snapHelper:SnapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(binding.slider)
 
         return binding.root
     }
